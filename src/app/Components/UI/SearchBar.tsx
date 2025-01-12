dbcbf97fbc79054cf4bb7edde10e162b9396603c
Bouhm <bbouhm@gmail.com>
2025-01-12 11:44:09 -0800

    update searchbar

--- src/app/Components/UI/SearchBar.tsx
+++ src/app/Components/UI/SearchBar.tsx
@@ -1,4 +1,5 @@
-import ".SearchBar.css";
+import { Icon } from '@iconify/react';
+import "./SearchBar.css";
 
 type SearchBarProps = {
   placeholder: string
@@ -7,8 +8,16 @@ type SearchBarProps = {
 export default function SearchBar({ placeholder }: SearchBarProps) {
   return (
     <div>
-        <input type="search" placeholder={placeholder} />
-        <button type="submit">Search</button>
+      <input className="rounded-full p-4" type="search" maxLength={24} placeholder={placeholder} />
+      <button className="rounded-full m-[-50px] align-middle w-12 h-12" type="submit">
+        <Icon 
+          className="text-center m-auto" 
+          icon="tabler:send" 
+          color="black" 
+          height="20" 
+          width="20" 
+        />
+      </button>
     </div>
   )
 }
